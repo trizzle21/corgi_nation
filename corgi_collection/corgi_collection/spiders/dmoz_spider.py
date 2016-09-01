3c3890e8531c3cf258044db580dd8385370464c7
trizzle21 <tdross21@gmail.com>
2016-08-31 22:56:30 -0700

    added simple incomplete spider

--- corgi_collection/corgi_collection/spiders/dmoz_spider.py
+++ corgi_collection/corgi_collection/spiders/dmoz_spider.py
@@ -3,9 +3,9 @@ import pillow
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
-    allowed_domains = []
+    allowed_domains = ["www.google.com"]
     start_urls = [
-
+    	"https://www.google.com/search?site=imghp&tbm=isch&source=hp&biw=1284&bih=625&q=corgi+puppies&oq=corgi&gs_l=img.1.1.0l10.2660.3340.0.4569.5.4.0.1.1.0.58.210.4.4.0....0...1ac.1.64.img..0.5.211.ye4ueTSq_iY#tbm=isch&q=corgi"
     ]
 
     def parse(self, response):
